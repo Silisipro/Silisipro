2d028825d9dea0c0793408a9d9bda274b98c9328
Eliezer <kanlihanou-eliezer.ago-koucou@epitech.eu>
2024-09-12 14:26:18 +0100

    widget meteo

--- frontenddashboardapp/src/App.js
+++ frontenddashboardapp/src/App.js
@@ -1,7 +1,15 @@
+import Navbar from './components/Navbar';
+import WeatherWidget from './components/WeatherWidget';
+import DateWidget from './components/DateWidget';
+
+
 export default function App() {
     return (
-      <h1 className="text-3xl font-bold underline text-red-600">
-        Hello world!
+      <h1 className="text-3xl font-bold underline ">
+            <Navbar/>
+            <WeatherWidget/>
+            <DateWidget/>
+            
       </h1>
     )
   }
